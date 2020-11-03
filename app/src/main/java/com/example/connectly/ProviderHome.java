e1266c7626a74f6dc4cb8ae3c4c1c9ca9b0a1acd
ZahraaMaher <43747635+ZahraaMaher@users.noreply.github.com>
2020-11-04 02:44:13 +0300

    Zahra Maher Provider Home

--- app/src/main/java/com/example/connectly/ProviderHome.java
+++ app/src/main/java/com/example/connectly/ProviderHome.java
@@ -2,13 +2,37 @@ package com.example.connectly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProviderHome extends AppCompatActivity {
+
+    Button PostJobBtn;
+    Button ViewRequestsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_home);
+
+        PostJobBtn=(Button) findViewById(R.id.PostJob_providerHome);
+        ViewRequestsBtn=(Button) findViewById(R.id.viewRequests_providerHome);
+
+
+
+    }
+
+    public void viewRequests_onClick(View view) {
+        Intent moveToPostJob = new Intent(this,PostJobActivity.class);
+        startActivity(moveToPostJob);
+
+
+    }
+
+    public void postJob_onClick(View view) {
+        Intent moveToViewRequests = new Intent(this,ViewRequsteActivity.class);
+        startActivity(moveToViewRequests);
     }
 }
