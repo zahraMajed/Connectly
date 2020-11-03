95fe02b794e913118e883ea43d81c2ce15f22e78
Fatima AlRamadan <2170005648@iau.edu.sa>
2020-11-03 03:30:19 +0300

    FatimaAlramadan commit

---
+++ app/src/main/java/com/example/connectly/ApplyJob.java
@@ -0,0 +1,14 @@
+package com.example.connectly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplyJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_job);
+    }
+}
