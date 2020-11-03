70bdbaeb3a27908cdb5be289ea12410d9e205e03
Fatima <fattiimma.aj@gmail.com>
2020-11-03 17:58:03 +0300

    add go to Post Job screen in login screen click logo

--- app/src/main/java/com/example/connectly/Login.java
+++ app/src/main/java/com/example/connectly/Login.java
@@ -2,7 +2,9 @@ package com.example.connectly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        // go to Post Job screen
+        findViewById(R.id.imageView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ViewRequsteActivity.class));
+            }
+        });
     }
 }
