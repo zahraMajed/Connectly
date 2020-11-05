259e9937789a510896623a85e84e85b240ab19a8
zahraMajed <zahraMajed.zahra_m.alzawad@hotmail.com>
2020-11-05 17:03:31 +0300

    commit

--- app/src/main/java/com/example/connectly/PostJobActivity.java
+++ app/src/main/java/com/example/connectly/PostJobActivity.java
@@ -1,6 +1,7 @@
 package com.example.connectly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +9,15 @@ import android.view.View;
 import android.widget.TextView;
 
 public class PostJobActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_job);
+        toolbar = findViewById(R.id.myToolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Post job");
 
         findViewById(R.id.postJob).setOnClickListener(new View.OnClickListener() {
             @Override
