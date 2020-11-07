8298049382757a2f2f10fa56a229d315096cd2db
ZahraaMaher <43747635+ZahraaMaher@users.noreply.github.com>
2020-11-07 03:19:32 +0300

    Zahra Maher Adding Fatimah Almomen's Post Job work

--- app/src/main/java/com/example/connectly/PostJobActivity.java
+++ app/src/main/java/com/example/connectly/PostJobActivity.java
@@ -1,5 +1,6 @@
 package com.example.connectly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -7,23 +8,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PostJobActivity extends AppCompatActivity {
+import java.io.PipedInputStream;
 
+public class PostJobActivity extends AppCompatActivity {
+    //declare the components:
+    Button postJobButton;
+    Button clearDataButton;
+    Button cancelButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_job);
 
-        findViewById(R.id.postJob).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(PostJobActivity.this,ViewRequsteActivity.class));
-            }
-        });
+        //define the buttons:
+        postJobButton=(Button) findViewById(R.id.postJob);
+        clearDataButton=(Button) findViewById(R.id.clearData);
+        cancelButton=(Button) findViewById(R.id.cancelPostJob);
+
+        //define the toolbar
+        toolbar=findViewById(R.id.myToolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Post Job");
+
+
+    }
+    //go to View Requests Activity by on click:
+    public void postJobHandler(View view) {
+        Intent goToViewRequests = new Intent(PostJobActivity.this,ViewRequsteActivity.class);
+        startActivity(goToViewRequests);
+    }//end of post job handler
+    //cancelationHandler
+    //go to Provider Home Activity by on click:
+    public void cancelationHandler(View view) {
+        Intent goToProviderHome = new Intent(PostJobActivity.this,ProviderHome.class);
+        startActivity(goToProviderHome);
     }
     //Attache menu
     @Override
@@ -32,4 +57,26 @@ public class PostJobActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu2, menu);
         return true;
     }
+
+    //on Click on menu items:
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if (id == R.id.menu2_home){
+            Intent intent= new Intent(PostJobActivity.this,ProviderHome.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.Menu2_profile){
+            Intent intent= new Intent(PostJobActivity.this,Company_profile.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.menu2_logout){
+            Intent intent= new Intent(PostJobActivity.this,Login.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
